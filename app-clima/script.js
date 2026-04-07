6f636a46a88d2f00b3ca3d0d945e01ffc31c3dec
Diego Herrera <diegoherreraruiz2@gmail.com>
2026-04-07 18:28:02 -0500

    Finalizacion de proyecto

--- app-clima/script.js
+++ app-clima/script.js
@@ -3,40 +3,67 @@ const resultado = document.querySelector("#resultado");
 const API_KEY = "a87ab4161cfc2bf2b03f414271030b99";
 
 async function getWeather() {
-    const city = nameCity.value.trim();
-    if (city === '') return
+  const city = nameCity.value.trim();
+  if (city === "") return;
 
-    resultado.textContent = 'Buscando...';
+  resultado.textContent = "Buscando...";
 
-    try {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(url)
-        console.log(data)
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(url);
+    console.log(data);
 
-        if (data.cod === 404) {
-            resultado.innerHTML = `
+    if (data.cod === 404) {
+      resultado.innerHTML = `
             <h2>Ciudad no encontrada</h2>
-            `
-            return;
-        } 
-
-        const w = data;
-        resultado.innerHTML = `
-        <h2>${w.name}</h2>
-        <p>${w.main.temp}</p>
-        <p>${w.weather[0].description}</p>
-        `
-
-    } catch (error) {
-        resultado.textContent = 'Error al obtener el clima';
+            `;
+      return;
     }
 
-    nameCity.addEventListener('keydown', (e) => {
-        if (e.key === 'Enter') {
-            getWeather();
-        }
-    })
+    const icon = data.weather[0].icon;
+    const iconURL = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
+    const w = data;
+    resultado.innerHTML = `
+        <div class="card">
+          <h2>${w.name}</h2>
+          <p> Temperature: ${w.main.temp}</p>
+          <p> Description: ${w.weather[0].description}</p>
+          <p>💧 Humedad: ${data.main.humidity}%</p>
+          <p>💨 Viento: ${data.wind.speed} m/s</p>
+        </div>
+        `;
+    
+    resultado.style.animation = "none";
+    resultado.offsetHeight;
+    resultado.style.animation = "fadeInResult 0.5s ease";
+  } catch (error) {
+    resultado.textContent = "Error al obtener el clima";
+  }
+}
+
+nameCity.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    getWeather();
+  }
+});
+
+const toggle = document.getElementById("toggleTheme");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  const isDark = document.body.classList.contains("dark");
+  updateIcon(isDark);
+});
+
+function updateIcon(isDark) {
+  const icon = document.getElementById("toggleTheme");
+  if (isDark) {
+    icon.textContent = "☀️";
+  } else {
+    icon.textContent = "🌙";
+  }
 }
