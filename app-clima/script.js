ae8f85c0345f041f8cdba69825f01c1af78b7cac
Diego Herrera <diegoherreraruiz2@gmail.com>
2026-04-07 17:17:50 -0500

    Se solucionan algunos error de codificacion (aca el programa funciona)

--- app-clima/script.js
+++ app-clima/script.js
@@ -6,16 +6,37 @@ async function getWeather() {
     const city = nameCity.value.trim();
     if (city === '') return
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-    console.log(url)
+    resultado.textContent = 'Buscando...';
 
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data)
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(url)
+        console.log(data)
 
-    if (data.cod === '404') {
+        if (data.cod === 404) {
+            resultado.innerHTML = `
+            <h2>Ciudad no encontrada</h2>
+            `
+            return;
+        } 
+
+        const w = data;
         resultado.innerHTML = `
-        <h2>Ciudad no encontrada</h2>
+        <h2>${w.name}</h2>
+        <p>${w.main.temp}</p>
+        <p>${w.weather[0].description}</p>
         `
-    } 
+
+    } catch (error) {
+        resultado.textContent = 'Error al obtener el clima';
+    }
+
+    nameCity.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            getWeather();
+        }
+    })
+
 }
